5a077c3d721f3486235955d8cf64b27fd04d3c70
YKcasa <131603850+YKcasa@users.noreply.github.com>
2023-11-28 00:55:21 +0900

    20231127

--- bbs/main/forms.py
+++ bbs/main/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Article
 
@@ -5,7 +6,24 @@ from .models import Article
 class ArticleForm(ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'author', 'theme', 'text', 'attached_file']
+        fields = ['title', 'author', 'theme', 'text', 'attached_file','status', 'passcode']
 
-    # def has_changed(self):
-    #     return super().has_changed()
+    def __init__(self, *args, **kwargs):
+        super(ArticleForm, self).__init__(*args, **kwargs)
+
+        if 'instance' not in kwargs or kwargs['instance'] is None:
+            self.fields['status'].widget = forms.HiddenInput()
+
+        if 'user' in kwargs and kwargs['user'].is_staff:
+            self.fields['status'].widget = forms.Select()
+            self.fields['status'].queryset = Article.STATUS_CHOICES
+
+    def validate_passcode(self):
+        passcode = self.cleand_data['passcode']
+        if not passcode:
+            return passcode
+        
+        if not passcode.isdigit() or len(passcode) != 4:
+            raise forms.ValidationError('4桁の半角数字を入力してください。')
+        
+        return passcode

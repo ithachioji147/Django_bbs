d12a41acf18a86d3207a8b270212a944fd141125
YKcasa <131603850+YKcasa@users.noreply.github.com>
2023-12-08 16:41:08 +0900

    settings/production temp-fix

--- bbs/bbs/settings/base.py
+++ bbs/bbs/settings/base.py
@@ -1,8 +1,6 @@
 from pathlib import Path
-import os
-import re
-from configparser import ConfigParser
 import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -54,8 +52,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bbs.wsgi.application'
 
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -84,47 +80,27 @@ USE_I18N = True
 USE_TZ = True
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
 ####################################
 # 以下、追加部分
 ####################################
 
-LOGIN_REDIRECT_URL = '/'
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
-
+# Cookieファイルの保存期間
 SESSION_COOKIE_AGE = 14 * 24 * 60 * 60
 
+# ログインしていない状態でのリダイレクト先
+LOGIN_REDIRECT_URL = '/'
 
-# ログインしていない状態でアクセスできるパス
+# ログインしていない状態でもアクセスできるパス
 ALLOWED_PATHS = [
     '/login/',
-    '/admin/'
+    '/admin/',  # うまく動かないので後で確認
 ]
 
-
-# 以下は設定ファイル（config.ini）から読み込む --- localのみ
-# config = ConfigParser()
-# config.read('config.ini')
-
-# SlackのWebhook --- local時の設定
-# SLACK_WEBHOOK_URL_STAFF = config.get('WEBHOOK_STAFF', 'URL')
-# SLACK_WEBHOOK_URL_GENERAL = config.get('WEBHOOK_GENERAL', 'URL')
-
-# SlackのWebhook --- 本番環境は環境変数を使用
-SLACK_WEBHOOK_URL_STAFF = os.environ.get('WEBHOOK_STAFF_URL')
-SLACK_WEBHOOK_URL_GENERAL = os.environ.get('WEBHOOK_GENERAL_URL')
-
-# NGROKで発行する一時的なアドレス
-# ALLOWED_HOSTS = ['localhost', config.get('NGROK', 'HOST')]
-# CSRF_TRUSTED_ORIGINS = [config.get('NGROK', 'FULL_URL')]
-
+# セッティングの振り分け？現状コメントアウトでも動きそう、不要であればあとで削除
 # django_heroku.settings(locals())
--- bbs/bbs/settings/production.py
+++ bbs/bbs/settings/production.py
@@ -1,29 +1,13 @@
 import os
-from .base import *
 import dj_database_url
-# from pathlib import Path
-# import re
-from configparser import ConfigParser
-# import django_heroku
+from .base import *
 
 
 DEBUG = False
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-
-
-# 静的ファイルはWhitenoiseを使用
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR, 'static']
 
 
 # データベースはHerokuのPostogresを使用
@@ -35,16 +19,24 @@ DATABASES = {
 }
 
 
+# 静的ファイルはWhitenoiseを使用
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+# 下記記述があるとデプロイ時のcollectstaticが失敗する？
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # メディアファイルはAWS S3を使用
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'ap-northeast-1'
 AWS_STORAGE_BUCKET_NAME = 'hachioji147-bbs-storage'
-
-# アクセスキー
+MEDIA_URL = 'https://hachioji147-bbs-storage.s3.amazonaws.com/'
+# AWSアクセスキー
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-MEDIA_URL = 'https://hachioji147-bbs-storage.s3.amazonaws.com/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+# Slack通知用のWebhook
+SLACK_WEBHOOK_URL_STAFF = os.environ.get('WEBHOOK_STAFF_URL')
+SLACK_WEBHOOK_URL_GENERAL = os.environ.get('WEBHOOK_GENERAL_URL')
